4d73f46b65e2b6af891b54765a658417a9617c15
Nick Gundobin <ngundobin@gmail.com>
2019-11-12 13:09:23 -0500

    Completed App
    This app will check if it is a good enough password to use.

--- checkmypassword.py
+++ checkmypassword.py
@@ -1,5 +1,11 @@
+'''
+The password will run through SHA1 hash of your string
+to check password correctness
+'''
+
 import requests
-import hashlib
+import hashlib # hashing algorithm i will use
+import sys
 
 def request_api_data(query_char: str)->str:
     url = 'https://api.pwnedpasswords.com/range/'+query_char
@@ -8,13 +14,31 @@ def request_api_data(query_char: str)->str:
         raise RuntimeError(f'Error fetching: {res.status_code}, check and try again')
     return res
 
+'''
+Loop through all the hashes
+'''
+def get_password_leaks_count(hashes,hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
 def pwned_api_check(password: str):
-    sha1password = hashlib.sha1(password.encode('utf-8'))
+    # Needs to be encoded in UTF-8, otherwise will throw error
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     #Save 5 chars of the password and in tail remaining chars
     first5_chars, tail = sha1password[:5],sha1password[5:]
     response = request_api_data(first5_chars)
-    print(first5_chars,tail)
-    return response
+    return get_password_leaks_count(response,tail)
 
-#request_api_data('123')
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times...you should probably change your password')
+        else:
+            print(f'{password} was not found. Carry on!')
+    return 'Done!'
+
+main(sys.argv[1:])
